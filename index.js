04e146c00aeacf9f51702762355956285c3ffd46
Gebruiker <gebruiker@MacBook-Pro-van-Sjoerd.local>
2017-11-09 11:17:35 +0100

    first commit

--- index.js
+++ index.js
@@ -1,22 +1,67 @@
+/*jslint devel: true */
 // JavaScript Document
 //aside toggle
 var aside = document.querySelector('aside');
-var button = document.querySelector('body > button');
+var asidebutton = document.querySelector('body > button');
 
 var open = function () {
+    'use strict';
+	aside.classList.toggle('open');
+};
 
-	aside.classList.toggle('open')
-}
+/*------------------------------------------------------*/
 
-button.addEventListener('click', open);
 
-//hart micro interactie [src="img/download.svg"]
+asidebutton.addEventListener('click', open);
+
 var gif = document.querySelector('[src="img/download.gif"]');
 var download = document.querySelector('[src="img/download.svg"]');
 
 var laden = function () {
+    'use strict';
+	gif.classList.toggle('laden');
+};
 
-	gif.classList.toggle('laden')
-}
+/*------------------------------------------------------*/
+
 
 download.addEventListener('click', laden);
+
+
+/*------------------------------------------------------*/
+
+console.log('opslaan');
+var opslaan = document.querySelector('#opslaan');
+var opslaan = document.getElementById('#opslaan');
+var opslaan = document.getElementById('opslaan').getAttribute('src', '/image/like.svg');
+var opslaan = document.querySelector('img');
+
+function OpslaanZichtbaar() {
+    'use strict';
+    opslaan.classList.toggle('OpslaanZichtbaar');
+}
+
+opslaan.addEventListener('click', function (event) {
+    'use strict';
+    event.preventDefault();
+//    Vergeet wat je 'default' zou doen en voert hide() uit
+    [OpslaanZichtbaar]();
+});
+
+var opslaan = document.getElementById('opslaan');
+
+opslaan.addEventListener('click', function (event) {
+    'use strict';
+	event.preventDefault();
+	OpslaanZichtbaar();
+});
+
+/*------------------------------------------------------*/
+
+/*----------------POPUP--------------------------------------*/
+
+
+
+
+
+
